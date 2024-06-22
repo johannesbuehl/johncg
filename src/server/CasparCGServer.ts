047871624e1b200d76d4058718990a8aa962c55f
Simon Ziegler <simon.ziegler.1998+github@pm.me>
2024-06-22 23:34:13 +0200

    Added console-output for casparcg-server

--- src/server/CasparCGServer.ts
+++ src/server/CasparCGServer.ts
@@ -35,12 +35,37 @@ export default class CasparCGServer {
 		logger.log(`Launching CasparCG-Server: '${this.server_path}'`);
 		this.server_process = ChildProcess.execFile(this.server_path, { cwd: this.casparcg_path });
 
+		// redirect the output to the console
+		this.server_process.stdout.on("data", (data: Buffer) => {
+			const lines = data.toString().replaceAll("\r", "").split("\n");
+
+			lines.forEach((line) => {
+				console.log(`[CasparCG] ${line}`);
+			});
+		});
+
+		this.server_process.stderr.on("data", (data: Buffer) => {
+			const lines = data.toString().replaceAll("\r", "").split("\n");
+
+			lines.forEach((line) => {
+				console.error(`[CasparCG] ${line}`);
+			});
+		});
+
 		this.server_process.addListener("close", () => this.launch_server());
 	}
 
 	launch_scanner() {
 		logger.log(`Launching CasparCG-Scanner: '${this.scanner_path}'`);
 		this.scanner_process = ChildProcess.execFile(this.scanner_path, { cwd: this.casparcg_path });
+
+		this.scanner_process.stderr.on("data", (data: Buffer) => {
+			const lines = data.toString().replaceAll("\r", "").split("\n");
+
+			lines.forEach((line) => {
+				console.error(`[CasparCG Scanner] ${line}`);
+			});
+		});
 
 		this.scanner_process.addListener("close", () => this.launch_scanner());
 	}
