d92526ee7da527da6488f58e1e1ee59234513bb8
Simon Ziegler <simon.ziegler.1998+github@pm.me>
2024-10-27 23:25:41 +0100

    renamed release run-script to not include version and platform

--- build/build-release.mjs
+++ build/build-release.mjs
@@ -14,10 +14,6 @@ const build_config = yaml.parse(fs.readFileSync(path.join(__dirname, "build_conf
 // set the config-number in the build-config and version.json
 const package_json = JSON.parse(fs.readFileSync(build_config.package_json ?? "package.json", "utf-8"));
 
-build_config.builds.server.script_path = `JohnCG_${package_json.version}_${process.platform}`;
-
-fs.writeFileSync(path.join(__dirname, "build_config.yaml"), yaml.stringify(build_config));
-
 fs.writeFileSync("src/version.ts", `export const Version = "${package_json.version}";\n`);
 
 esbuild.build({
