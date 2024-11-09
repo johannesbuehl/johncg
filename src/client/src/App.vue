91ee204b0ac13440c6c8194b464572d56b7bace4
Simon Ziegler <simon.ziegler.1998+github@pm.me>
2024-11-09 15:05:05 +0100

    fixed some icons not working

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -10,6 +10,8 @@
 <script setup lang="ts">
 	import { ref, watch } from "vue";
 	import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+	import { library } from "@fortawesome/fontawesome-svg-core";
+	import * as fas from "@fortawesome/free-solid-svg-icons";
 
 	import ControlWindow from "@/ControlWindow/ControlWindow.vue";
 	import { ControlWindowState } from "./Enums";
@@ -20,6 +22,8 @@
 	import * as JCGPSend from "@server/JCGPSendMessages";
 	import type * as JCGPRecv from "@server/JCGPReceiveMessages";
 	import type { ItemFileMap, ItemFileMapped, ItemNodeMapped } from "@server/search_part_types";
+
+	library.add(fas.faCheck, fas.faFloppyDisk, fas.faTrash, fas.faXmark);
 
 	// eslint-disable-next-line @typescript-eslint/naming-convention
 	const Config = {
@@ -394,7 +398,7 @@
 					>
 						<FontAwesomeIcon
 							v-if="option.icon !== undefined"
-							:icon="option.icon"
+							:icon="['fas', option.icon]"
 							:squre="option.text === undefined"
 						/>{{ option.text ?? "" }}
 					</MenuButton>
